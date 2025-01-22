42bc1701b64f9e78b242531ea71d54d04f0a3983
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-01-22 18:37:30 +0530

    keyboard combinations

--- BackTracking/practice/keyboard.java
+++ BackTracking/practice/keyboard.java
@@ -1,3 +1,36 @@
 public class keyboard{
-    
+    public static char L[][]={{},{},{'a','b','c'}
+    ,{'d','e','f'},
+    {'g','h','i'},
+    {'j','k','l'},
+    {'m','n','o'},
+    {'p','q','r','s'},
+    {'t','u','v'},
+    {'w','x','y','z'}
+    };
+    public static void lettercombinations(String D){
+        int length = D.length();
+        if(length == 0){
+            System.out.println("");
+            return;
+        }
+        bfs(0,length,new StringBuilder(),D);
+    }
+    public static void bfs(int position,int length,StringBuilder sb , String D){
+        if(position==length){
+            System.out.println(sb.toString());
+
+        }else{
+            char [] letters = L[Character.getNumericValue(D.charAt(position))];
+            System.out.println(letters);
+
+             for(int i=0 ; i<letters.length ; i++){
+                 bfs(position+1, length, new StringBuilder(sb).append(letters[i]), D);
+             }
+         }
+         
+    }
+    public static void main(String[] args) {
+        lettercombinations("2");
+    }
 }
