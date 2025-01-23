4d6ae926d67e18fc6c2823db058585169d8cd762
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-01-23 15:30:29 +0530

    basics of arraylist

---
+++ ArrayList/Basics.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+
+public class Basics{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+    }
+}
