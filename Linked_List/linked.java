7eb38ea57a2ebeeccbeca69b54c52d7316d39ff9
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-01-30 16:06:54 +0530

    linked list basics

---
+++ Linked_List/linked.java
@@ -0,0 +1,23 @@
+
+import java.util.LinkedList;
+
+public class linked{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){ //constructor
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        //for adding and removing elements we will create methods
+    }
+
+}
