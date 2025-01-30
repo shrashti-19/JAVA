b4217041290773de0b40195f518e22f6eadc4e16
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-01-30 16:32:14 +0530

    linked list adding elements on the first

---
+++ Linked_List/linked2.java
@@ -0,0 +1,38 @@
+
+
+public class linked2{
+    public class Node{
+        int data;
+        Node next;
+
+        public Node(int data){ //constructor
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        //check if ll is empty
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+        }
+        // rest steps 
+        newNode.next = head;
+        head = newNode;
+        
+
+    }
+
+    
+    public static void main(String[] args) {
+        
+        linked2 list = new linked2();
+        list.addFirst(1);
+        list.addFirst(2);
+        
+        
+    }
+}
