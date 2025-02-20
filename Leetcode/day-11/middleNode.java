27aab2a12fe47776fecdc34bdb903855775836f1
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-02-20 17:29:16 +0530

    linked list middle node

---
+++ Leetcode/day-11/middleNode.java
@@ -0,0 +1,31 @@
+public class middleNode{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static Node middleNodell(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        System.out.println(middleNodell(head).data);
+        
+    }
+}
