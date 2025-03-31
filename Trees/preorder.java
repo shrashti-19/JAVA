e04ed0763db0d1fa28d038a5e7710ac4812e4202
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-03-31 15:22:03 +0530

    postorder traversal

--- Trees/preorder.java
+++ Trees/preorder.java
@@ -40,12 +40,21 @@ public class preorder{
         System.out.println(root.data + " ");
         inorder(root.right);
     }
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data + " ");
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         //System.out.println(root.data);
-       // preorder(root);
-        inorder(root);
+        //preorder(root);
+        //inorder(root);
+        postorder(root);
     }
 }
