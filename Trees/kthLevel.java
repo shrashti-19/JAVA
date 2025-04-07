7bf8d0591f588ed9f18bc696179a7a93d115b691
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-04-07 17:30:17 +0530

    kth level iteratively

--- Trees/kthLevel.java
+++ Trees/kthLevel.java
@@ -1,3 +1,5 @@
+
+import java.util.*;
 public class kthLevel{
     static class Node{
         int data;
@@ -11,18 +13,49 @@ public class kthLevel{
         }
     }
 
-    public static void kthLevelprint(Node root,int level,int k){
+    public static void kthLevelprint(Node root,int k){
+        // if(root==null){
+        //     return;
+        // }
+        // if(level == k){
+        //     System.out.println(root.data + " ");
+        //     return;
+        // }
+        // kthLevelprint(root.left, level+1, k);
+        // kthLevelprint(root.right, level+1, k);
+        int level = 1;
         if(root==null){
             return;
         }
-        if(level == k){
-            System.out.println(root.data + " ");
-            return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            if(level == k){
+                
+                for(int i=0 ; i<size ; i++){
+                    Node curr = q.poll();
+                    System.out.println(curr.data + " ");
+                }
+                System.out.println();
+                return;
+            }
+            //next level
+            for(int i=0 ; i<size ; i++){
+                Node curr = q.poll();
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+            level++;
         }
-        kthLevelprint(root.left, level+1, k);
-        kthLevelprint(root.right, level+1, k);
 
     }
+
+    
     public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
@@ -31,8 +64,8 @@ public class kthLevel{
         root.right = new Node(3);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        int level =1 ;
+        
         int k = 2;
-        kthLevelprint(root,level,k);
+        kthLevelprint(root,k);
     }
 }
