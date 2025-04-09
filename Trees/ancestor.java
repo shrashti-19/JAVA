05f1a5a556f6d112a87c89dec4c6cdee3ee3cec4
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-04-09 18:41:53 +0530

    ancestor finding approach 2 with O(n)

--- Trees/ancestor.java
+++ Trees/ancestor.java
@@ -51,6 +51,27 @@ public class ancestor{
         path.remove(path.size()-1);
         return false;
     }
+    public static Node lca2(Node root , int n1, int n2){
+        if(root == null){
+            return root;
+        }
+
+        if(root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node leftlca = lca2(root.left,n1,n2);
+        Node rightlca = lca2(root.right , n1,n2);
+        //case when right is null return left 
+        if(rightlca == null){
+            return leftlca;
+        }
+        if(leftlca==null){
+            return rightlca;
+        }
+
+        //if both are not null then root
+        return root;
+    }
     public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
@@ -62,5 +83,6 @@ public class ancestor{
         int n1=4;
         int n2=5;
         System.out.println(lca(root, n1, n2).data);
+        System.out.println(lca2(root,n1,n2).data);
     }
 }
