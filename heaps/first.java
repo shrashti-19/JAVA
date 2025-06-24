31eff97b8d87999bb29bd6dab5de9f4028af81dc
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-06-24 14:30:08 +0530

    minimum element from the heap and also completed main method

--- heaps/first.java
+++ heaps/first.java
@@ -5,7 +5,7 @@ public class first{
     static class heap{
         ArrayList<Integer> arr = new ArrayList<>();
 
-        void add(int data){
+        public void add(int data){
             arr.add(data);
 
             //data is stored in the last position in the array list.
@@ -16,10 +16,31 @@ public class first{
                 int temp = arr.get(x);
                 arr.set(x, arr.get(parent));
                 arr.set(parent, temp);
+
+                x= parent;
+                parent = (x-1)/2;
             }
         }
+
+        public int peek(){
+            return arr.get(0);
+        }
+
+        public void print(){
+            System.out.println("Current heap: " + arr);
+        }
     }
+
     public static void main(String[] args) {
-        
+        heap minHeap = new heap();
+
+        minHeap.add(10);
+        minHeap.add(5);
+        minHeap.add(20);
+        minHeap.add(3);
+
+        minHeap.print();
+
+        System.out.println("Minimum element from the heap is: "+ minHeap.peek());
     }
 }
