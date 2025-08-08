281196cbf05443562d4716b61e3906ae61bc9e3d
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-08-08 18:32:28 +0530

    iterator

--- hashing/hashset.java
+++ hashing/hashset.java
@@ -4,18 +4,26 @@ public class hashset {
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet<>();
         set.add(1);
-        set.add(2);
+        set.add(12);
         set.add(5);
+        set.add(2);
+        set.add(8);
+        set.add(9);
 
-        System.out.println(set);
-        set.remove(2);
-
-        if(set.contains(2)){
-            System.out.println("set contains 2");
+        Iterator it = set.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
         }
-        set.clear();
-        System.out.println(set.size());
-        System.out.println(set.isEmpty());
+
+        // System.out.println(set);
+        // set.remove(2);
+
+        // if(set.contains(2)){
+        //     System.out.println("set contains 2");
+        // }
+        // set.clear();
+        // System.out.println(set.size());
+        // System.out.println(set.isEmpty());
     }
     
 
