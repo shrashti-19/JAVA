e4bd4f7bb40187bc2b09752b2545e20d390c34d2
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-08-08 18:26:26 +0530

    hashset - unordered collection, unique

---
+++ hashing/hashset.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(5);
+
+        System.out.println(set);
+        set.remove(2);
+
+        if(set.contains(2)){
+            System.out.println("set contains 2");
+        }
+        set.clear();
+        System.out.println(set.size());
+    }
+    
+
+    
+}
