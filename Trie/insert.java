e52ce3638e6ec45cc624dbc901006c9d5245cdfa
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-08-23 14:18:47 +0530

    searching in trie

--- Trie/insert.java
+++ Trie/insert.java
@@ -21,10 +21,27 @@ public class insert{
         }
         current.eow = true; //word end
     }
+
+    public static boolean search(String key){
+        Node current = root;
+
+        for(int level =0 ; level<key.length() ; level++){
+            int idx = key.charAt(level)-'a';
+            if(current.array[idx]==null){
+                return false;
+            }
+            current = current.array[idx];
+        }
+        return current.eow == true;
+    }
     public static void main(String args[]){
         String words[] = {"the", "a", "their", "there", "any", "thee"};
         for(int i=0 ; i<words.length ; i++){
             insert(words[i]);
         }
+
+        System.out.println(search("the"));
+        System.out.println(search("an"));
+
     }
 }
