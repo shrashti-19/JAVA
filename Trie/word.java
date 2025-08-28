cd942515f70165359cb14e7e54379e32be648994
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-08-28 11:59:01 +0530

    longest word with all it's prefixes in the dictionary present

---
+++ Trie/word.java
@@ -0,0 +1,53 @@
+public class word{
+    public static class Node{
+        Node[] children = new Node[26];
+        boolean eow = false;
+
+        public Node(){
+            for(int i=0 ; i<26 ; i++){
+                children[i]=null;
+            }
+        }
+    }
+    public static Node root = new Node();
+    public static void insert(String word){
+        Node curr = root;
+        for(int i = 0; i < word.length(); i++){
+            int index = word.charAt(i) - 'a';
+            if(curr.children[index] == null){
+                curr.children[index] = new Node();
+            }
+            curr = curr.children[index];
+        }
+        curr.eow = true;
+    }
+    public static String finalAns = " ";
+    public static void longestWord(Node root, StringBuilder temp){
+        if(root == null) return;
+        
+
+        for(int i=25 ; i>=0 ; i--){
+            if(root.children[i]!=null && root.children[i].eow==true){
+                char ch = (char)(i+'a');
+                temp.append(ch);
+
+                if(temp.length() > finalAns.length()){
+                    finalAns = temp.toString();
+                }
+
+                longestWord(root.children[i], temp);
+
+                temp.deleteCharAt(temp.length()-1); //last character delete
+            }  
+        }
+
+    }
+    public static void main(String args[]){
+        String words[] = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+        for(int i=0 ; i<words.length ; i++){
+            insert(words[i]);
+        }
+        longestWord(root, new StringBuilder(" "));
+        System.out.println(finalAns);
+    }
+}
